191a570a2d640be34c8794959ec0f067d9e3f7ef
esraasera <esraam9756@gmail.com>
2025-08-14 19:07:06 +0300

    new updates

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
